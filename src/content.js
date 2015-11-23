e270d2e0e215fcd52606fd73c4d42b26d2cd044e
Joel Griffith <jgriffith@appnexus.com>
2015-11-23 11:16:31 -0800

    State proxy is wired

--- src/content.js
+++ src/content.js
@@ -1,6 +1,3 @@
-console.log('CONTENT.js');
-
-
 // For reference
 // document.addEventListener('state-buddy', function(data) {
 // 	switch (data.detail.type) {
@@ -13,29 +10,11 @@ console.log('CONTENT.js');
 // 	}
 // });
 
-function receiveMessage(event) {
-	console.log('CONTENT: Event', event);
-}
-window.addEventListener('message', receiveMessage, false);
+window.addEventListener('message', function (event) {
+	chrome.runtime.sendMessage({ type: event.data.type, state: event.data.state });
+}, false);
 
-
-chrome.runtime.onMessage.addListener(
-  function (request, sender, sendResponse) {
-		console.log('CONTENT: FORWARD', request);
-		// Forward messages from background extension
-		window.postMessage(request, '*');
-		// console.log(request, sender, sendResponse);
-    // if (request.type === 'GET_STATE' ) {
-		//
-    //   console.log('CONTENT', 'firstHref ****');
-		//
-		// 	// const evt = document.createEvent('CustomEvent');
-		// 	// evt.initCustomEvent('state-buddy', true, true, { type: 'GET_STATE' });
-		// 	// document.dispatchEvent(evt);
-		// 	window.postMessage({ type: 'GET_STATE' }, '*');
-		//
-    //   // This line is new!
-    //   chrome.runtime.sendMessage({'type': 'SEND_STATE', 'state': { mystate: 5 } });
-    // }
+chrome.runtime.onMessage.addListener(function(request) {
+	window.postMessage(request, '*');
   }
 );
